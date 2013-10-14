3de39dec9a2708034ce92bb24877c8c0ce174491
Kujbor <Kujbor@ya.ru>
2013-10-14 16:09:39 +0400

    Update example.js

--- example.js
+++ example.js
@@ -1,16 +1,16 @@
+$.get('templates/pagination.html', function(template) {
+
+    $.paginator.setTemplate(_.template(template));
+
+});
+
 $(function() {
 
-    $.get('templates/pagination.html', function(template) {
-
-        $.paginator.setTemplate(_.template(template));
-
-        $('#pagination').paginator({
-            base: 'page/',
-            first: 1,
-            current: 3,
-            last: 10
-        });
-
+    $('#pagination').paginator({
+        base: 'page/',
+        first: 1,
+        current: 3,
+        last: 10
     });
 
 });

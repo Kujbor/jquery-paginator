d7db4c0898a68b495f2756444073ccf7bdb53d50
Kujbor <Kujbor@ya.ru>
2013-10-14 13:43:53 +0400

    Create example.js

---
+++ example.js
@@ -0,0 +1,6 @@
+$(function() {
+    $.get('templates/pagination.html', function(template) {
+        $.paginator.setTemplate(_.template(template));
+        $('#pagination').paginator({first: '/1.html', last: '2.html'});
+    });
+});

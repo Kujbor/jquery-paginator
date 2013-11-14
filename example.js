01b859e03b05ee6aebcda157a49993e85875681c
Kujbor <Kujbor@ya.ru>
2013-11-14 17:45:07 +0300

    Update example.js

--- example.js
+++ example.js
@@ -1,21 +1,21 @@
 /*
- * example.js - example of jQuery paginator plugin setup
+ * Example of jQuery paginator plugin setup
  * Author Oleg Taranov aka Kujbor
  * Copyright (C) 2013: CubeComp Development
  */
-$.get('templates/pagination.html', function(template) {
-
-    $.paginator.setTemplate(_.template(template));
-
-});
-
 $(function() {
 
-    $('#pagination').paginator({
-        base: 'page/',
-        first: 1,
-        current: 3,
-        last: 10
+    $.get('templates/pagination.html', function(template) {
+
+        $.paginator.setTemplate(_.template(template));
+
+        $('#pagination').paginator({
+            base: 'page/',
+            first: 1,
+            current: 3,
+            last: 10
+        });
+
     });
 
 });
